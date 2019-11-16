6416d4081d66536614c161574f85ac357627ff97
blaise <bdecarne@gmail.com>
2019-11-16 12:36:24 +0100

    add webhook security

--- Controller/WebhookController.php
+++ Controller/WebhookController.php
@@ -25,9 +25,37 @@ class WebhookController extends Controller
         if (!isset($requestData->id) || !isset($requestData->object)) {
             throw new BadRequestHttpException('Invalid webhook request data');
         }
+
+        // If event id ends with 14 zero's then it is a test webhook event. Return 200 status.
+        if(substr($requestData->id, -14 ) == "00000000000000"){
+            return new Response('Webhook test successful', 200);
+        }
+
         if ($requestData->object !== StripeObjectType::EVENT) {
             throw new StripeException('Unknown stripe object type in webhook');
         }
+
+        // test secure
+        $webhookSecret = $this->getParameter('miracode_stripe.webhook_secret');
+        if($webhookSecret !== null) {
+            $sigHeader = $request->headers->get('Stripe-Signature');
+           try {
+               $event = \Stripe\Webhook::constructEvent(
+                   $request->getContent(), $sigHeader, $webhookSecret
+               );
+           } catch(\UnexpectedValueException $e) {
+               // Invalid payload
+               throw new \Stripe\StripeException(
+                   sprintf('Invalid event payload', $requestData->id)
+               );
+           } catch(SignatureVerification $e) {
+               // Invalid signature
+               throw new \Stripe\StripeException(
+                   sprintf('Invalid event signature', $requestData->id)
+               );
+           }
+        }
+
         $stripeEventApi = new StripeEventApi();
         if (!$stripeEventObject = $stripeEventApi->retrieve($requestData->id)) {
             throw new StripeException(
